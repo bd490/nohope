c3a359e9dbf9eb9e3073e53c3ca840110920a585
Manwinder Paul <93801811+Pokesweg@users.noreply.github.com>
2023-11-02 10:59:31 -0400

    Update dataup.php

--- 490 Project/demo/dataup.php
+++ 490 Project/demo/dataup.php
@@ -9,57 +9,65 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $username = $_POST['Uname'];
 $password = $_POST['Pword'];
-$Firstname = $_POST['Fname'];
+$FirstName = $_POST['Fname'];
 $LastName = $_POST['Lname'];
 $college = $_POST['College'];
 $major = $_POST['Major'];
 
 
-#$sql = "INSERT INTO finalTWO ( userID, userPw, userfirstname, userlastname, usercollege, usermajor) VALUES ('$username','$password', '$FirstName','$LastName','$college','$major')";
+//$sql = "INSERT INTO finalTWO ( userID, userPw, userfirstname, userlastname, usercollege, usermajor) VALUES ('$username','$password', '$FirstName','$LastName','$college','$major')";
 
-#$rs = mysqli_query($conn, $sql);
+//$rs = mysqli_query($conn, $sql);
 
-#if($rs){
-#	header("Location: fauxlogin.html");
-#}
-#else{
-#	echo "somethings wrong";
-#}
-
-
-require_once('/var/www/gci/sub/path.inc');
-require_once('/var/www/gci/sub/get_host_info.inc');
-require_once('/var/www/gci/sub/rabbitMQLib.inc');
-
-
-//$client = new rabbitMQClient("/home/danny/git/rabbitmqphp_example/testRabbitMQ.ini","testServer");
-//if (isset($argv[1]))
-//{
-  //$msg = $argv[1];
+//if($rs){
+//	header("Location: fauxlogin.html");
 //}
-//else
-//{
- // $msg = "test message there are bugs under your skin";
+//else{
+//	echo "somethings wrong";
 //}
 
-//$request = array();
-//$request['type'] = "Login";
+
+require_once('sub/path.inc');
+require_once('sub/get_host_info.inc');
+require_once('sub/rabbitMQLib.inc');
+
+
+
+$client = new rabbitMQClient("sub/testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message there are bugs under your skin";
+}
+
+$request = array();
+$request['type'] = "Login";
 //$request['username'] = "gyro";
 //$request['password'] = "joestar";
 //$request['message'] = $msg;
-//$response = $client->send_request($request);
-//$response = $client->publish($request);
-
-//echo "client received response: ".PHP_EOL;
-//print_r($response);
-//echo "\n\n";
-
-//echo $argv[0]." END".PHP_EOL;
-
+$request['username'] = $username;
+$request['password'] = $password;
+$request['message'] = $msg;
+$request['first'] = $FirstName;
+$request['last'] = $LastName;
+$request['college'] = $college;
+$request['major'] = $major;
 
 
-//echo "before creation comes destruction";
 
+
+
+$response = $client->send_request($request);
+$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
 
 
 ?>
