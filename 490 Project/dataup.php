7c9eecf049926f07a359185a9230ce9c55a05585
Manwinder Paul <93801811+Pokesweg@users.noreply.github.com>
2023-11-30 09:56:55 -0500

    Update dataup.php
    added basic hashing

--- 490 Project/dataup.php
+++ 490 Project/dataup.php
@@ -1,13 +1,30 @@
 <?php
+
+//require_once('path.inc');
+//require_once('get_host_info.inc');
+//require_once('rabbitMQLib.inc');
+
+//$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+//if (isset($argv[1]))
+//{
+//  $msg = $argv[1];
+//}
+//else
+//{
+//  $msg = "test message";
+//}
+
+
+
 $servername = "sql1.njit.edu";// you need to put your assigned server name
 $username = "dsg2";// your ucid
 $password = "3aWorldisUltimat3!";// database password
 $dbname = "dsg2"; // your ucid is your database name
 
 
-//$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-//$conn = mysqli_connect("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+
 
 $username = $_POST['Uname'];
 $password = $_POST['Pword'];
@@ -16,8 +33,28 @@ $LastName = $_POST['Lname'];
 $college  = $_POST['College'];
 $major    = $_POST['Major'];
 
-$sql = "INSERT INTO finalTWO ( userID,userPW, userfirstname, userlastname,usercollege,usermajor) VALUES
-( '$username','$password', '$FirstName','$LastName','$college','$major')";
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+//$request = array();
+//$request['usernm'] = $username;
+//$request['pword'] = "$password;
+//$request['fname'] = $FirstName;
+//$request['lname'] = $LastName;
+//$request['college'] = "$college;
+//$request['major'] = $major;
+//$request['message'] = $msg;
+//$response = $client->send_request($request);
+
+
+//echo "client received response: ".PHP_EOL;
+//print_r($response);
+//echo "\n\n";
+
+//echo $argv[0]." END".PHP_EOL;
+
+$sql = "INSERT INTO finalTHREE ( userID,userPW, userfirstname, userlastname,usercollege,usermajor, hashPW) VALUES
+( '$username','$password', '$FirstName','$LastName','$college','$major','$hashed_password')";
 
 $rs = mysqli_query($conn, $sql);
 
@@ -30,4 +67,3 @@ else{
 echo "somethings wrong";
 }
 ?>
-
