6d33591a0fcbdfe930df4aea0e1fe15591d3b58c
Manwinder Paul <93801811+Pokesweg@users.noreply.github.com>
2023-12-20 01:13:15 -0500

    Add files via upload

---
+++ final/search3.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Search</title>
+    <link href="signupstyle.css" rel="stylesheet">
+</head>
+<!-- php to acquire variables-->
+
+<?php
+$servername = "sql1.njit.edu";// you need to put your assigned server name
+$username = "dsg2";// your ucid
+$password = "3aWorldisUltimat3!";// database password
+$dbname = "dsg2"; // your ucid is your database name
+
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+session_start();
+
+$username= $_SESSION["username"];
+
+
+
+
+$sql2 = "SELECT `userfirstname` FROM `finalTWO` WHERE `userID`='$username'";
+$query2 = mysqli_query($conn,$sql2);
+$result2 = mysqli_fetch_assoc($query2);
+$resultstring2 = $result2['userfirstname'];
+
+
+$sql3 = "SELECT `userlastname` FROM `finalTWO` WHERE `userID`='$username'";
+$query3 = mysqli_query($conn,$sql3);
+$result3 = mysqli_fetch_assoc($query3);
+$resultstring3 = $result3['userlastname'];
+
+
+$type= $_POST['type'];
+
+$sql = "SELECT * FROM dokkan WHERE type LIKE '%$type%'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+while($row = $result->fetch_assoc() ){
+	echo $row["name"]."  ".$row["rarity"]."  ".$row["class"]." ".$row["type"]."<br>";
+}
+} else {
+	echo "0 records";
+}
+
+$conn->close();
+
+$rs = mysqli_query($conn, $sql);
+
+
+if($rs){
+
+header("Location: Todo.php");
+
+}
+else{
+echo "somethings wrong";
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
